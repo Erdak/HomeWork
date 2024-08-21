88e801e88be0782a986e93fbe1e52477decd4153
Erdak <d.erdenedavaa@gmail.com>
2024-08-21 15:54:25 +1000

    Homework

---
+++ Homework_1_1.py
@@ -0,0 +1,8 @@
+q1 = input("Enter your name: ") 
+print(q1 + '. Nice to meet you!')
+
+q2=input("How old are you? ")
+print("your age is: " + q2 + '. super!')
+
+q3=input("Where are you from? ")
+print("You are from " + q3 + '. Nice!')
